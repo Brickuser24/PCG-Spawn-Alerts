389241ac98226e3a8495df0dbb52c65c2b3b2419
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-06-03 11:46:02 +0530

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+#User Data
+email=""
+#App Password for your email
+password=""
+
+#Alert Settings
+a_tier_alerts=True
+s_tier_alerts=True
+starter_alerts=True
+
+#Pokemon you want alerts for
+pokemon =[58,64,72,73,82,93,101,112,133,176,207,221,227,319,348,364,375,408,423,444,536,552,554,566,596,597,608,611,620,624,629,680,703,712,713,747,767,846,870,882,885]
+#Any type of Pokemon you want alerts for
+types=["flying"]
